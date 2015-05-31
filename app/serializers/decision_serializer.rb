d19b4b81c07f6f4bf220236e9a48f380cf3eee68
Logan Price & Nathaniel Clapp <pair+logandsprice.nclapp@devbootcamp.com>
2015-05-30 20:12:30 -0500

    take out proposals from the decision serializer

--- app/serializers/decision_serializer.rb
+++ app/serializers/decision_serializer.rb
@@ -1,5 +1,5 @@
 class DecisionSerializer < ActiveModel::Serializer
-  attributes :context, :is_active, :proposals
+  attributes :context, :is_active#, :proposals
   # has_many :participations
   # has_many :proposals
   has_many :participants
