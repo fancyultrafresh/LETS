134b827943ed5d8b05026de7ff05b6b764432cd0
Logan Price & Nathaniel Clapp <pair+logandsprice.nclapp@devbootcamp.com>
2015-05-31 14:27:32 -0500

    add create decision and participation functionality

--- app/controllers/api/decisions_controller.rb
+++ app/controllers/api/decisions_controller.rb
@@ -16,8 +16,24 @@ class Api::DecisionsController < ApplicationController
     render json: Decision.find(params[:id]).proposals.find_by_status("open")
   end
 
+  def create
+    user = User.find(params[:user_id])
+    decision = user.decisions.new(decision_params)
+
+    if decision.save
+      Participation.create(user: user, decision: decision)
+      head 200
+    else
+      head 500
+    end
+  end
+
   def default_serializer_options
     { root: false }
   end
 
+  private
+  def decision_params
+    params.require(:decision).permit(:context)
+  end
 end
--- app/controllers/api/participations_controller.rb
+++ app/controllers/api/participations_controller.rb
@@ -8,12 +8,28 @@ class Api::ParticipationsController < ApplicationController
     # render json: Participation.where(user_id: params[:user_id])
   end
 
-  def show
+  def create
+    user = User.find_by_email(params[:user][:email])
+    if user
+      user.decisions << Decision.find(params[:decision_id])
+      head 200
+    else
+      head 455
+    end
+  end
+
+  def destroy
 
   end
 
   def default_serializer_options
     { root: false }
   end
+
+
+  # private
+  # def participation_params
+  #   params.require(:participation).permit(:email,:phone_number)
+  # end
 
 end
