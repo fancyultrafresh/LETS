4bc6f4bfe5bce619ea71350b8430290986473bd6
Logan Price & Nathaniel Clapp <pair+logandsprice.nclapp@devbootcamp.com>
2015-05-31 17:13:34 -0500

    validates_presence_of :status, :proposed_idea, :participation_id, and inclusion of status commented out, in Proposal model

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -3,4 +3,7 @@ class Proposal < ActiveRecord::Base
   has_one    :proposer, through: :participation, source: :user
   has_one    :decision, through: :participation
   has_many   :queries
+
+  validates_presence_of :status, :proposed_idea, :participation_id
+  # validates_inclusion_of :status, :in => %w(yes no open canceled)
 end
