04a4c3598609eafa062d0c6cf288eac37906ebaa
Logan Price & Nathaniel Clapp <pair+logandsprice.nclapp@devbootcamp.com>
2015-06-01 20:15:20 -0500

    commit and pull request final model/controller changes before rails app pivot

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -5,4 +5,17 @@ class Decision < ActiveRecord::Base
   # has_one  :winning_proposal, through: :participations, source: :proposal
 
   validates_presence_of :context
+
+
+  def active_proposal
+    active_proposals = self.proposals.where(status:"open")
+    if active_proposals.length == 1
+      active_proposals[0]
+    else
+      errors[:base] <<"This decision either has more than one activer proposal or has no active proposals"
+    end
+  end
+
+
+
 end
--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -10,6 +10,27 @@ class Proposal < ActiveRecord::Base
     @_decision ||= self.participation.decision
   end
 
+  def participations
+    @_participations ||= self.decision.participations
+  end
+
+  def voters
+    @_voters ||= self.queries.pluck(:participation_id).sort.map {|id| Participation.find(id)}
+  end
+
+  def not_yet_voted
+
+  end
+
+  def ordered_participations
+
+  end
+
+  def next_voter
+
+  end
+
+
   validates_presence_of :status, :proposed_idea, :participation_id
   # validates_inclusion_of :status, :in => %w(accepted refused open)
 
